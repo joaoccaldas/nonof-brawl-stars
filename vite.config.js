6817e83992a97a826a1285defa25c46fa59c70a9
joaoccaldas <joaoccaldas@gmail.com>
2026-05-10 14:38:36 +0200

    Add Vite configuration for project setup

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+  base: '/nonof-brawl-stars/',
+      root: 'build',
+      build: {
+    outDir: 'dist'
+      },
+        server: {
+    port: 8765,
+          host: true
+      }
+}
